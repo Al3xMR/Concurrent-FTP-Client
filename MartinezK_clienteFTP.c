23a63efc660eec0d15928f2f9a4b8d91e131eddb
Kevin Martinez <kevinprincipal9@gmail.com>
2025-11-09 21:10:57 +0000

    Login Successful

--- MartinezK_clienteFTP.c
+++ MartinezK_clienteFTP.c
@@ -32,29 +32,56 @@ int main(int argc, char *argv[]){
 		}
 	}
 	
-	// Master socket
+	// -*-*-*-*-*-*-* Master socket -*-*-*-*-*-*-*-*
 	
 	int sd = connectTCP(srv_add, port);
 
 	sd >= 0 ? printf("Socket created with file descriptor: %d\n", sd) : printf("Error to create the socket\n");
 
 	char buf[500];
+	char msg[600];
 
 	ssize_t bytes_recv = recv(sd, buf, sizeof(buf), 0);
+	ssize_t bytes_sent;
 
 	bytes_recv > 0 ? printf("Server response: %s\n", buf) : printf("Server did not respond\n");
-	
+
+	// ------------- Username read -----------------	
 	memset(buf, 0, sizeof(buf));
 	printf("USER: ");
 	fflush(stdout);
-	bytes_recv = read(0, buf, sizeof(buf));
-	printf("User received: %s\n", buf);
+	bytes_recv = read(0, buf, sizeof(buf) - 1);
+	buf[bytes_recv] = '\0';
 
+	// ------------- Username formatting ----------
+	buf[strcspn(buf, "\r\n")] = '\0';
+	snprintf(msg, sizeof(msg), "USER %s\r\n", buf);
+
+	// ------------- Username sent to server ------
+	bytes_sent = send(sd, msg, strlen(msg), 0);
 	memset(buf, 0, sizeof(buf));
-	printf("PASSWORD: ");
-	fflush(stdout);
-	bytes_recv = read(0, buf, sizeof(buf));
-	printf("Pass received: %s\n", buf);
+	bytes_recv = recv(sd, buf, sizeof(buf) - 1, 0);
+	buf[bytes_recv] = '\0';
+	printf("SERVER ANSWER: %s\n", buf);
+
+	// ------------- Password read -----------------
+        memset(buf, 0, sizeof(buf));
+	memset(msg, 0, sizeof(msg));
+        printf("PASSWORD: ");
+        fflush(stdout);
+        bytes_recv = read(0, buf, sizeof(buf) - 1);
+        buf[bytes_recv] = '\0';
+
+        // ------------- Password formatting ----------
+        buf[strcspn(buf, "\r\n")] = '\0';
+        snprintf(msg, sizeof(msg), "PASS %s\r\n", buf);
+
+        // ------------- Password sent to server ------
+        bytes_sent = send(sd, msg, strlen(msg), 0);
+        memset(buf, 0, sizeof(buf));
+        bytes_recv = recv(sd, buf, sizeof(buf) - 1, 0);
+	buf[bytes_recv] = '\0';
+        printf("SERVER ANSWER: %s\n", buf);
 
 	//while(1) {
 
