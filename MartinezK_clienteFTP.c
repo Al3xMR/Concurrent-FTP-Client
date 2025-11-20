f072da2cb11ce604d791ccadce1202c149638ed9
Kevin Martinez <kevinprincipal9@gmail.com>
2025-11-20 17:01:58 -0500

    Preparation for store

--- MartinezK_clienteFTP.c
+++ MartinezK_clienteFTP.c
@@ -8,6 +8,8 @@
 int connectTCP(const char *host, const char *service );
 
 void readFromShell( char *buf);
+void storeFile( int sd, bool passive, const char *filepath);
+int send_file_over_socket(int data_sock, const char *filepath);
 
 typedef struct {
     char ip[32];
@@ -93,8 +95,6 @@ int main(int argc, char *argv[]){
 		buf[bytes_recv] = '\0';
 		printf("SERVER ANSWER: %s\n", buf);
 
-
-
 		// -------------- Validation ------------------
 		if (buf[0] == '2') {
 			break;
@@ -102,18 +102,13 @@ int main(int argc, char *argv[]){
 	}
 
 
-	// ---------------- Transfer mode ---------------
-
-	printf("Passive mode: on\n");
-	PassiveInfo info;
-	info.ok = false;
-	info.ip[0] = '\0';
-	info.port = 0; 
-	info = enterPassiveMode(sd);
-	printf("\nIP:%s\nPuerto:%d\n", info.ip, info.port);
 	// ------------- Prompt ----------------------------
 	readFromShell(buf);
 	printf("%s", buf);
+
+	//if (strcmp(buf, "store")) {
+		storeFile( sd, true, "/home/kmartinez/archivosDelCliente/pruebaEnvioFTP.txt");
+	//}
 
 	return 0;
 }
@@ -129,7 +124,7 @@ void readFromShell( char *buf) {
 		buf[bytes_recv] = '\0';
         	buf[strcspn(buf, "\r\n")] = '\0';
 
-		if (!strcmp(buf, "list")) {
+		if (!strcmp(buf, "store")) {
 			validInput = true;
 		} else {
 			printf("Insert a valid input\n");
@@ -164,4 +159,57 @@ PassiveInfo enterPassiveMode(int sd) {
 	info.port = p1 * 256 + p2;
 	info.ok = true;
 	return info;
+}
+
+void storeFile( int sd, bool passive, const char *filepath) {
+	int pid = fork();
+	if (pid = 0) {
+		char msg[64];
+        	char buf[256];
+        	ssize_t bytes_sent, bytes_recv;
+        	FILE *fp = fopen(filepath, "rb");
+		if (passive) {
+			PassiveInfo info = enterPassiveMode(sd);
+			char port[2];
+			snprintf(port, sizeof(port), "%d", info.port);
+			int nsd = connectTCP(info.ip, port);
+			snprintf(msg, sizeof(msg), "STOR %s\r\n", filepath);
+			printf("\nMensaje de guardado enviado: %s\n", msg);
+		}
+	}
+
+}
+
+int send_file_over_socket(int data_sock, const char *filepath) {
+    FILE *fp = fopen(filepath, "rb");
+    if (!fp) {
+        perror("Error abriendo archivo");
+        return -1;
+    }
+
+    char buffer[500];
+    size_t bytes_read;
+
+    while ((bytes_read = fread(buffer, 1, 500, fp)) > 0) {
+
+        size_t total_sent = 0;
+        while (total_sent < bytes_read) {
+
+            ssize_t sent = send(data_sock,
+                                buffer + total_sent,
+                                bytes_read - total_sent,
+                                0);
+
+            if (sent < 0) {
+                perror("Error enviando datos");
+                fclose(fp);
+                return -1;
+            }
+
+            total_sent += sent;
+        }
+    }
+
+    fclose(fp);
+    return 0;  // éxito
 }
